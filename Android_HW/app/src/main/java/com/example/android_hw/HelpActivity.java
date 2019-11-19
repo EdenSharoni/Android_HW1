6768d036d70334131feba4584ab79289472f3fae
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-11-19 11:22:34 +0200

    xml

--- Android_HW/app/src/main/java/com/example/android_hw/HelpActivity.java
+++ Android_HW/app/src/main/java/com/example/android_hw/HelpActivity.java
@@ -1,13 +1,29 @@
 package com.example.android_hw;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView back;
+    private Vibrator vibe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        back = findViewById(R.id.backBtn);
+        back.setOnClickListener(this);
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        vibe.vibrate(20);
+        finish();
     }
 }
