0c75762c93ed126095daedfde0879cf0e5791207
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-11-21 00:17:35 +0200

    Update GameActivity.java

--- Android_HW/app/src/main/java/com/example/android_hw/GameActivity.java
+++ Android_HW/app/src/main/java/com/example/android_hw/GameActivity.java
@@ -3,20 +3,18 @@ package com.example.android_hw;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
     private static final String TAG = GameActivity.class.getSimpleName();
     private LinearLayout linearLayoutManager;
     private float height;
     private float legoHeight;
-    private int loop;
-    private int amountOfLegoColumn = 4;
+    private int screenHeightDividedByLegoSize;
+    private int amountOfLegoColumn = 6;
+    private LinearLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +24,22 @@ public class GameActivity extends AppCompatActivity {
         getScreenHeightDividedByLegoSize();
         initLinearLayoutManager();
 
+        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
         for (int i = 0; i < amountOfLegoColumn; i++) {
+
             LinearLayout linearLayout = new LinearLayout(this);
-            linearLayout.setWeightSum(1);
             linearLayout.setOrientation(LinearLayout.VERTICAL);
             linearLayout.setGravity(Gravity.CENTER);
-            linearLayoutManager.addView(linearLayout);
-            for (int j = 0; j < loop; j++) {
+            linearLayout.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
+
+            for (int j = 0; j < screenHeightDividedByLegoSize; j++) {
                 ImageView imageView = new ImageView(this);
-                LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                imageView.setLayoutParams(lparams);
+                imageView.setLayoutParams(layoutParams);
                 imageView.setBackground(getResources().getDrawable(R.drawable.blue_lego));
                 linearLayout.addView(imageView);
             }
+            linearLayoutManager.addView(linearLayout);
         }
         setContentView(linearLayoutManager);
     }
@@ -54,6 +55,6 @@ public class GameActivity extends AppCompatActivity {
     private void getScreenHeightDividedByLegoSize() {
         height = Resources.getSystem().getDisplayMetrics().heightPixels;
         legoHeight = getResources().getDrawable(R.drawable.blue_lego).getMinimumHeight();
-        loop = Math.round(height/legoHeight);
+        screenHeightDividedByLegoSize = Math.round(height / legoHeight);
     }
 }
