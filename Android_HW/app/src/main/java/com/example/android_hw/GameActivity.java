0553e79b8f4398206a9de9c2cd4f97974ec2048f
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2019-11-21 17:15:14 +0200

    Runnable

--- Android_HW/app/src/main/java/com/example/android_hw/GameActivity.java
+++ Android_HW/app/src/main/java/com/example/android_hw/GameActivity.java
@@ -3,16 +3,20 @@ package com.example.android_hw;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,21 +26,38 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private float height;
     private float legoHeight;
     private int screenHeightDividedByLegoSize;
-    private int amountOfLegoColumn = 3;
-    private ImageView imageView;
+    private int amountOfLegoColumn = 5;
+    private ImageView lego;
+    private ImageView player;
     private LinearLayout linearLayout;
     private LinearLayout linearLayoutManager;
     private Button button;
+    private int playerCurrentPosition;
+    private int legoCurrentPosition = 0;
+    private ArrayList<ImageView> playerArrayList = new ArrayList<>();
+    private ArrayList<ImageView> legoArrayList = new ArrayList<>();
+    private int lives = 3;
+    private Random random;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         getScreenHeightDividedByLegoSize();
         initFrameLayoutManager();
+        initLives();
         initLinearLayoutWithLegoBlocks();
         initButton();
         initPlayer();
+        tickEndlessly();
         setContentView(frameLayoutManager);
+    }
+
+    private void initLives() {
+        TextView textView = new TextView(this);
+        textView.setText("Lives: " + lives);
+        textView.setTextColor(Color.BLACK);
+        textView.setTextSize(30f);
+        frameLayoutManager.addView(textView);
     }
 
     private void initFrameLayoutManager() {
@@ -49,16 +70,17 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         initLinearLayoutManager(linearLayoutManager);
 
         for (int i = 0; i < amountOfLegoColumn; i++) {
-
             linearLayout = new LinearLayout(this);
             initLinearLayout(linearLayout, Gravity.CENTER);
             setColor();
 
             for (int j = 0; j < screenHeightDividedByLegoSize; j++) {
-                ImageView imageView = new ImageView(this);
-                imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                imageView.setBackground(drawable);
-                linearLayout.addView(imageView);
+                lego = new ImageView(this);
+                lego.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                lego.setBackground(drawable);
+                lego.setVisibility(View.INVISIBLE);
+                legoArrayList.add(lego);
+                linearLayout.addView(lego);
             }
             linearLayoutManager.addView(linearLayout);
         }
@@ -72,7 +94,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 0; i < 2; i++) {
             linearLayout = new LinearLayout(this);
             initLinearLayout(linearLayout, Gravity.CENTER);
-
             button = new Button(this);
             button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
             button.setBackgroundColor(Color.TRANSPARENT);
@@ -86,6 +107,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initPlayer() {
+        playerCurrentPosition = amountOfLegoColumn / 2;
+
         linearLayoutManager = new LinearLayout(this);
         initLinearLayoutManager(linearLayoutManager);
 
@@ -94,24 +117,24 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             linearLayout = new LinearLayout(this);
             initLinearLayout(linearLayout, Gravity.CENTER | Gravity.BOTTOM);
 
-            imageView = new ImageView(this);
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            imageView.setBackground(getResources().getDrawable(R.drawable.player_lego));
-            imageView.setId(j);
+            player = new ImageView(this);
+            player.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            player.setBackground(getResources().getDrawable(R.drawable.player_lego));
+            player.setId(j);
+            playerArrayList.add(player);
+            if (player.getId() == amountOfLegoColumn / 2)
+                player.setVisibility(View.VISIBLE);
+            else
+                player.setVisibility(View.INVISIBLE);
 
-            //if (imageView.getId() == amountOfLegoColumn / 2)
-            //    imageView.setVisibility(View.VISIBLE);
-            // else
-            //     imageView.setVisibility(View.INVISIBLE);
-
-            linearLayout.addView(imageView);
+            linearLayout.addView(player);
             linearLayoutManager.addView(linearLayout);
         }
         frameLayoutManager.addView(linearLayoutManager);
     }
 
     private void setColor() {
-        Random random = new Random();
+        random = new Random();
         int i = random.nextInt(5 - 0);
         switch (i) {
             case 0:
@@ -152,12 +175,54 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(getApplicationContext(), "button " + v.getId(), Toast.LENGTH_SHORT).show();
-        switch (v.getId()){
-            case 0: //right
+        player = playerArrayList.get(playerCurrentPosition);
+        switch (v.getId()) {
+            case 0: //left
+                if (playerCurrentPosition != 0) {
+                    player.setVisibility(View.INVISIBLE);
+                    playerCurrentPosition--;
+                    player = playerArrayList.get(playerCurrentPosition);
+                    player.setVisibility(View.VISIBLE);
+                }
                 break;
-            case 1: //left
+            case 1: //right
+                if (playerCurrentPosition != (amountOfLegoColumn - 1)) {
+                    player.setVisibility(View.INVISIBLE);
+                    playerCurrentPosition++;
+                    player = playerArrayList.get(playerCurrentPosition);
+                    player.setVisibility(View.VISIBLE);
+                }
                 break;
         }
+    }
+
+    private void tickEndlessly() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    random = new Random();
+                    legoGame();
+                    tick();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void legoGame() {
+        //legoCurrentPosition = random.nextInt(2 - 0);
+        legoCurrentPosition++;
+        Log.e(TAG, "legoGame: " + legoCurrentPosition);
+        lego = legoArrayList.get(legoCurrentPosition);
+        lego.setVisibility(View.VISIBLE);
+    }
+
+    private void tick() {
+        Log.d(TAG, "ticking");
     }
 }
