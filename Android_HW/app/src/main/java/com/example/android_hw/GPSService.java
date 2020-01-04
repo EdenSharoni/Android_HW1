e63dc8f4b4f016be7c4ea1ef106515597554dc37
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2020-01-04 19:37:21 +0200

    GPS LocationListener

--- Android_HW/app/src/main/java/com/example/android_hw/GPSService.java
+++ Android_HW/app/src/main/java/com/example/android_hw/GPSService.java
@@ -1,99 +1,80 @@
 package com.example.android_hw;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import static android.location.LocationManager.GPS_PROVIDER;
 
-public class GPSService {
+public class GPSService implements LocationListener {
 
     private static final String TAG = GPSService.class.getSimpleName();
-    private static final int REQUEST_GPS = 1;
-    private LocationManager locationManager;
-    private List<Address> addresses;
+    LocationManager locationManager;
     private Location myLocation;
 
-    public GPSService(MainActivity context) {
-        getLocation(context);
-    }
-
-    public void getLocation(MainActivity context) {
+    @SuppressLint("MissingPermission")
+    public GPSService(Context context) {
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        myLocation = getLastKnownLocation(context);
+        locationManager.requestLocationUpdates(GPS_PROVIDER, 0, 0f, this);
+        isLocationEnabled(context);
+    }
 
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            buildAlertMessageNoGps(context);
-        }
+    private void isLocationEnabled(Context context) {
 
-        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-
-        addresses = null;
-        try {
-            if (myLocation != null) {
-                addresses = geocoder.getFromLocation(myLocation.getLatitude(), myLocation.getLongitude(), 1);
-            } else {
-                Log.e(TAG, "MyLocation is null");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (addresses == null) {
-            Log.e(TAG, "Waiting for Location");
-        } else {
-            if (addresses.size() > 0) {
-                Log.e(TAG, addresses.get(0).getFeatureName() + ", " + addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() + ", " + addresses.get(0).getCountryName());
-            }
+        if (!locationManager.isProviderEnabled(GPS_PROVIDER)) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            context.startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
         }
     }
 
-    private Location getLastKnownLocation(MainActivity context) {
-        List<String> providers = locationManager.getProviders(true);
-        Location bestLocation = null;
-        for (String provider : providers) {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_GPS);
-            }
-            Location l = locationManager.getLastKnownLocation(provider);
-            if (l == null) {
-                continue;
-            }
-            if (bestLocation == null) {
-                bestLocation = l;
-            }
-        }
-        return bestLocation;
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e(TAG, "onLocationChanged: ");
+        myLocation = location;
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        String msg = "New Latitude: " + latitude + "New Longitude: " + longitude;
+        Log.e(TAG, "onLocationChanged: " + msg);
+        //Toast.makeText(mContext, msg, Toast.LENGTH_LONG).show();
     }
 
-    private void buildAlertMessageNoGps(MainActivity context) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        context.startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        dialog.cancel();
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.e(TAG, "onStatusChanged: ");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.e(TAG, "onProviderEnabled: ");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.e(TAG, "onProviderDisabled: ");
     }
 
     public Location getMyLocation() {
--- Android_HW/app/src/main/java/com/example/android_hw/MainActivity.java
+++ Android_HW/app/src/main/java/com/example/android_hw/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.android_hw;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +22,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,7 +32,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import static android.location.LocationManager.GPS_PROVIDER;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
 
     private final String TAG = "MainActivity";
 
@@ -41,14 +43,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int SETTINGS_REQUEST_CODE = 2;
     private final int POP_UP_NAME_REQUEST_CODE = 4;
 
-
     private FirebaseUser user;
     private FirebaseFirestore db;
     private User localUser;
     private Intent intent;
-    private GPSService gpsService;
     private FirebaseAuth mAuth;
-
+    private Location myLocation;
 
     @BindView(R.id.gameOverTitle)
     ImageView gameOverTitle;
@@ -76,10 +76,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_GPS);
         } else {
+            locationManager.requestLocationUpdates(GPS_PROVIDER, 20000, 20f, this);
             findUser();
         }
     }
@@ -88,24 +90,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_GPS) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                findUser();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         }
     }
 
     private void findUser() {
-        gpsService = new GPSService(MainActivity.this);
         if (user == null) {
             mAuth.signInAnonymously()
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                user = mAuth.getCurrentUser();
-                                localUser = new User(user.getUid(), "", 0, true, 80, getString(R.string.screen), gpsService.getMyLocation().getLatitude(), gpsService.getMyLocation().getLongitude());
-                                highestScoreText.setText(String.format("%s %s", getString(R.string.highest), getString(R.string.score, localUser.getScore())));
-                                setUserDB();
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            user = mAuth.getCurrentUser();
+                            Log.e("GPS", "onComplete: ");
+                            if (myLocation == null) {
+                                localUser = new User(user.getUid(), "", 0, true, 80, getString(R.string.screen), 0, 0);
+                            } else {
+                                localUser = new User(user.getUid(), "", 0, true, 80, getString(R.string.screen), myLocation.getLatitude(), myLocation.getLongitude());
                             }
+                            highestScoreText.setText(String.format("%s %s", getString(R.string.highest), getString(R.string.score, localUser.getScore())));
+                            setUserDB();
                         }
                     });
         } else {
@@ -116,19 +120,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void setUserDB() {
         progressBar.setVisibility(View.VISIBLE);
         db.collection("Users")
-                .document(user.getUid()).set(localUser).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                progressBar.setVisibility(View.GONE);
-                checkNameValid();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                progressBar.setVisibility(View.GONE);
-                Log.e(TAG, getString(R.string.error_saving_data));
-            }
-        });
+                .document(user.getUid()).set(localUser).addOnSuccessListener(aVoid -> {
+                    progressBar.setVisibility(View.GONE);
+                    checkNameValid();
+                }).addOnFailureListener(e -> {
+                    progressBar.setVisibility(View.GONE);
+                    Log.e(TAG, getString(R.string.error_saving_data));
+                });
     }
 
     private void checkNameValid() {
@@ -150,19 +148,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             progressBar.setVisibility(View.GONE);
                             localUser = documentSnapshot.toObject(User.class);
                             if (localUser == null) {
-                                localUser = new User(user.getUid(), user.getDisplayName(), 0, true, 80, getString(R.string.screen), gpsService.getMyLocation().getLatitude(), gpsService.getMyLocation().getLongitude());
+                                Log.e("GPS", "getUserFromDB: ");
+                                if (myLocation == null) {
+                                    localUser = new User(user.getUid(), user.getDisplayName(), 0, true, 80, getString(R.string.screen), 0, 0);
+                                } else {
+                                    localUser = new User(user.getUid(), user.getDisplayName(), 0, true, 80, getString(R.string.screen), myLocation.getLatitude(), myLocation.getLongitude());
+                                }
                                 setUserDB();
                             } else {
+                                if (myLocation != null) {
+                                    localUser.setLatitude(myLocation.getLatitude());
+                                    localUser.setLongitude((myLocation.getLongitude()));
+                                }
                                 checkNameValid();
                             }
                             highestScoreText.setText(String.format("%s %s", getString(R.string.highest), getString(R.string.score, localUser.getScore())));
                         }
-                ).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                progressBar.setVisibility(View.GONE);
-            }
-        });
+                ).addOnFailureListener(e -> progressBar.setVisibility(View.GONE));
     }
 
     @Override
@@ -231,5 +233,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         help.setEnabled(bool);
         highestScore.setEnabled(bool);
         settings.setEnabled(bool);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        myLocation = location;
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        if (localUser != null) {
+            localUser.setLongitude(longitude);
+            localUser.setLatitude(latitude);
+            setUserDB();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)))
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
